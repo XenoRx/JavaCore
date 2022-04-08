eeabb5afcc60e3a3a305ab1ea1eacea3689ec3d4
disappear <disappear123@mail.ru>
2022-04-08 23:49:38 +0300

    Homework: 3 done

--- src/main/java/course2/homework3/Phonebook.java
+++ src/main/java/course2/homework3/Phonebook.java
@@ -1,31 +1,60 @@
 package course2.homework3;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class Phonebook {
-	private Map<Integer, String> phonebook;
+	private final Map<String, Set<Integer>> phonebook;
 	
 	Phonebook() {
 		phonebook = new HashMap<>();
 	}
 	
-	public void add(int number, String lastname) {
-		phonebook.put(number, lastname);
-	}
-	public void get(String lastname){
-		if(phonebook.containsValue(lastname)){
-			Set<Map.Entry<Integer,String>> set = phonebook.entrySet();
-			for(Map.Entry<Integer, String> m:set){
-				if (m.getValue().equals(lastname)){
-					System.out.println(m.getValue()+" - "+m.getKey());
-				}
-			}
-		} else {
-			System.out.println("Name not found");
+	public void add(String lastname, Integer phoneNumber) {
+		Set<Integer> number = new HashSet<>();
+		number.add(phoneNumber);
+		if (!phonebook.containsKey(lastname)){
+			phonebook.put(lastname, number);
+		}else {
+			final Set<Integer> newNumber = phonebook.get(lastname);
+			newNumber.add(phoneNumber);
 		}
 	}
 	
+	public Set<Integer> get(String lastname) {
+		return phonebook.get(lastname);
+	}
 }
+/*
+public class Phonebook {
+	private String lastname;
+	private int number;
+	private final ArrayList<Phonebook> phonebook = new ArrayList<>();
+	public Phonebook(){
+	}
+	public Phonebook(String lastname, Integer number) {
+		this.lastname = lastname;
+		this.number = number;
+	}
+	
+	public void add(String lastname, Integer number){
+		phonebook.add(new Phonebook(lastname, number));
+	}
+	
+	public ArrayList<Integer> get(String name){
+		ArrayList<Integer> numbers = new ArrayList<>();
+		for (Phonebook pb: phonebook) {
+			if (pb.getLastname().equalsIgnoreCase(name)) {
+				numbers.add(pb.getNumber());
+			}
+		}
+		return numbers;
+	}
+	
+	public String getLastname() {
+		return lastname;
+	}
+	
+	public Integer getNumber() {
+		return number;
+	}
+}*/
--- src/main/java/course2/homework3/hw3App.java
+++ src/main/java/course2/homework3/hw3App.java
@@ -1,22 +1,40 @@
 package course2.homework3;
 
+import java.util.HashSet;
+
 public class hw3App {
+	private static final String[] names = {
+			"Ivanov", "Alekseeva", "Nishin", "Davidov",
+			"Samueva", "Inaev", "Samueva",
+			"Kashin", "Sincev", "Ivanov"
+	};
+	
 	public static void main(String[] args) {
 		Phonebook phonebook = new Phonebook();
-		phonebook.add(233456655, "Ivanov");
-		phonebook.add(123123123, "Alekseeva");
-		phonebook.add(345543543, "Nishin");
-		phonebook.add(686767686, "Davidov");
-		phonebook.add(747664746, "Samueva");
-		phonebook.add(553665767, "Inaev");
-		phonebook.add(978765095, "Samueva");
-		phonebook.add(987799088, "Kashin");
-		phonebook.add(887888888, "Sincev");
-		phonebook.add(777677667, "Ivanov");
-		
-		phonebook.get("Ivanov");
-		phonebook.get("Samueva");
-		
+		phonebook.add("Ivanova",9129374);
+		phonebook.add("Nikolaev",78178238);
+		phonebook.add("Sincov",123123123);
+		phonebook.add("Ivanova",84757463);
+		phonebook.add("Pumba",3334455);
+		phonebook.add("Purumba",5554433);
+		System.out.println(output(names));
+		String name = "Ivanova";
+		System.out.println(name + " : " + phonebook.get(name));
 	}
-	
+	public static HashSet<String> output(String[] names) {
+		final HashSet<String> set = new HashSet<>();
+		for (String s : names) {
+			int count = 0;
+			if (!set.contains(s)) {
+				set.add(s);
+				for (String text : names) {
+					if (s.equalsIgnoreCase(text)) {
+						count++;
+					}
+				}
+				System.out.println(s + " - " + count);
+			}
+		}
+		return set;
+	}
 }
