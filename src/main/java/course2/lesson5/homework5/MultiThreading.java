0f877197980bb17dd3d5a09e2314bbecd2f614ee
disappear <disappear123@mail.ru>
2022-04-09 15:35:52 +0300

    Homework: 5 in process/1

--- src/main/java/course2/lesson5/homework5/MultiThreading.java
+++ src/main/java/course2/lesson5/homework5/MultiThreading.java
@@ -1,18 +1,26 @@
 package course2.lesson5.homework5;
 
 public class MultiThreading {
+	public static void main(String[] args) {
+		MultiThreading mt = new MultiThreading();
+		mt.func1(SIZE);
+		mt.func2(SIZE);
+	}
+	
 	static final int SIZE = 10000000;
 	static final int HALF = SIZE / 2;
 	float[] arr = new float[SIZE];
 	
 	public float[] count(float[] arr) {
 		for (int i = 0; i < arr.length; i++) {
-			arr[i] = (float) (arr[i] * Math.sin(0.2f + arr[i] / 5) * Math.cos(0.2f + arr[i] / 5) * Math.cos(0.4f + arr[i] / 2));
+			arr[i] = (float) (arr[i] * Math.sin(0.2f + arr[i] / 5)
+					* Math.cos(0.2f + arr[i] / 5)
+					* Math.cos(0.4f + arr[i] / 2));
 		}
 		return arr;
 	}
 	
-	public void oneThread() {
+	public void func1(int SIZE) {
 		float[] arr = new float[SIZE];
 		for (int i = 0; i < arr.length; i++) {
 			arr[i] = 1.0f;
@@ -23,7 +31,7 @@ public class MultiThreading {
 		
 	}
 	
-	public void secondThread() {
+	public void func2(int SIZE) {
 		float[] arr = new float[SIZE];
 		float[] arr1 = new float[HALF];
 		float[] arr2 = new float[HALF];
@@ -33,26 +41,14 @@ public class MultiThreading {
 		long startTime = System.currentTimeMillis();
 		System.arraycopy(arr, 0, arr1, 0, HALF);
 		System.arraycopy(arr, HALF, arr2, 0, HALF);
-		new Thread() {
-			public void run() {
-				float[] f1 = count(arr1);
-				System.arraycopy(f1, 0, arr1, 0, f1.length);
-			}
-		}.start();
-		new Thread() {
-			public void run() {
-				float[] f2 = count(arr2);
-				System.arraycopy(f2, 0, arr2, 0, f2.length);
-			}
-		}.start();
+		
+		Thread t1 = new Thread(() -> count(arr1));
+		Thread t2 = new Thread(() -> count(arr2));
+		
 		System.arraycopy(arr1, 0, arr, 0, HALF);
 		System.arraycopy(arr2, 0, arr, HALF, HALF);
 		System.out.println("Второй поток завершился за " + (System.currentTimeMillis() - startTime) + " ms");
-	}
-	
-	public static void main(String[] args) {
-		MultiThreading mt = new MultiThreading();
-		mt.oneThread();
-		mt.secondThread();
+		t1.start();
+		t2.start();
 	}
 }
